9fa25de3c859f74bafaf72fc6efe1b792983ae93
cy-song <ja345611@gmail.com>
2018-03-20 23:40:19 +0800

    改用老師的單一單字 念五次的音檔

--- end_point_all.m
+++ end_point_all.m
@@ -1,39 +1,18 @@
 clc ;clear;close all
 
-i=1;
+i=3;
 %% end point detection all
-for studnum=18:34
+for times=1:5
 
-if studnum>9
-    opt1=1;    
-else 
-    opt1=0;
-end
-    studnum
-    if opt1==0 %學生數字是1~9
-        if i<10
-            [y2,sr] =audioread([ 'C:\Users\jenny\Desktop\克 抗戰鐵路\105-2\paper 實驗 AHG\414 同學\handoff_0314-20170331T082045Z-001\handoff_0314\S0',num2str(studnum),'/S0',num2str(studnum),'A0',num2str(i),'-01.wav']);
-        else
-            [y2,sr] =audioread([ 'C:\Users\jenny\Desktop\克 抗戰鐵路\105-2\paper 實驗 AHG\414 同學\handoff_0314-20170331T082045Z-001\handoff_0314\S0',num2str(studnum),'/S0',num2str(studnum),'A',num2str(i),'-01.wav']);
-
-        end
-        
-    else
-        if i<10
-            [y2,sr] =audioread([ 'C:\Users\jenny\Desktop\克 抗戰鐵路\105-2\paper 實驗 AHG\414 同學\handoff_0314-20170331T082045Z-001\handoff_0314\S',num2str(studnum),'/S',num2str(studnum),'A0',num2str(i),'-01.wav']);
-        else
-            [y2,sr] =audioread([ 'C:\Users\jenny\Desktop\克 抗戰鐵路\105-2\paper 實驗 AHG\414 同學\handoff_0314-20170331T082045Z-001\handoff_0314\S',num2str(studnum),'/S',num2str(studnum),'A',num2str(i),'-01.wav']);
-
-        end
-        
-        
-    end
-%soundsc(y2,sr)
+     % {  
+    [y2,sr] =audioread([ 'C:\Users\jenny\Desktop\endpoint detection all/t1W0',num2str(i),'-0',num2str(times),'.wav']);
+ 
+%}
 
 %% student
 y2(:,2)=[];
 y1=y2;
-soundsc(y1,sr)
+% soundsc(y1,sr)
 
 %% new EPD
   [staPT, endPT] = EPD(y2,sr,1)
@@ -41,8 +20,8 @@ soundsc(y1,sr)
 %   pause
   soundsc(y1,sr)
 time6=(endPT-staPT)/sr;
-studnum=studnum-1;
-wavefilea=['C:\Users\jenny\Desktop\endpoint detection all\a',num2str(studnum),'.wav'];%%%%%save 
+% studnum=studnum-1;
+wavefilea=['C:\Users\jenny\Desktop\endpoint detection all\t1_cat_',num2str(times),'.wav'];%%%%%save 
  nbits=16;
  audiowrite(wavefilea,y1,sr);
 
